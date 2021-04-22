9efe256f928c1bf9414dd91dba7ccecc0a1e4a3f
Dario-Felipe <dariofelipe91@gmail.com>
2021-04-22 18:58:00 -0300

    feat(navigation): add new component for app navigate

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Main from './src/pages/Main/index';
+import { StatusBar } from 'expo-status-bar';
+import Navigation from './src/navigation/index';
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <Main />
+      <Navigation />
     </>
   );
 }

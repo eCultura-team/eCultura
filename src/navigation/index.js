eb2aabff4f4e20c014fa10d765980d65d2f382ee
Dario-Felipe <dariofelipe91@gmail.com>
2021-04-23 20:06:49 -0300

    feat(navigation): add exit app button and style for all header

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {
+  createStackNavigator,
+  HeaderBackButton,
+} from '@react-navigation/stack';
+import { BackHandler } from 'react-native';
+import { colors } from '../tokens';
 import Main from '../pages/Main';
 
 const Stack = createStackNavigator();
 
+const NavigationStyle = {
+  headerStyle: {
+    elevation: 0,
+    backgroundColor: colors.backgroundPage,
+  },
+};
+
 const Navigation = () => (
   <>
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Main} />
+      <Stack.Navigator initialRouteName="Home" screenOptions={NavigationStyle}>
+        <Stack.Screen
+          name="Home"
+          component={Main}
+          options={{
+            headerLeft: () => (
+              <HeaderBackButton onPress={BackHandler.exitApp} />
+            ),
+            title: '',
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   </>

f33aa1cc2d83e7ba11ec7a500ba6b7c9024aa51b
Dario-Felipe <dariofelipe91@gmail.com>
2021-04-26 20:33:07 -0300

    refactor(tokens): add new color gray

--- src/tokens/colors.js
+++ src/tokens/colors.js
@@ -2,12 +2,14 @@ const primary = '#4C3891';
 const secondary = '#6BCDD2';
 const darkGreen = '#53B5BA';
 const backgroundPage = '#F0F0F5';
+const gray = '#6C6C80';
 
 const colors = {
   primary,
   secondary,
   darkGreen,
   backgroundPage,
+  gray,
 };
 
 export default colors;

773e8d95d195ab2ed29a448f437b45402c8890fe
Dario-Felipe <dariofelipe91@gmail.com>
2021-04-26 20:34:11 -0300

    feat(welcome): add new component for welcome app title

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { MainContent } from './style';
 import Hello from '../../components/Hello/index';
+import Welcome from '../../components/Welcome';
 
 const Main = () => (
   <>
+    <Welcome />
     <MainContent>
       <Hello />
     </MainContent>

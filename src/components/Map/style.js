7fd06c74610f30200c1a2e75a657590b802195a6
Dario-Felipe <dariofelipe91@gmail.com>
2021-04-27 19:29:56 -0300

    style(map): adjust map height

--- src/components/Map/style.js
+++ src/components/Map/style.js
@@ -3,14 +3,15 @@ import styled from 'styled-components/native';
 export const MapContainer = styled.View`
   align-items: center;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   margin-top: 16px;
   width: 100%;
 `;
 
 export const MapContent = styled.View`
   border-radius: 10px;
-  height: 480px;
+  height: 100%;
+  max-height: 70%;
   overflow: hidden;
   width: 345px;
 `;

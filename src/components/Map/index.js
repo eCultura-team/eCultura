69781aaa7c622bee8caf7c6911a2296aa3e2b9d3
Dario-Felipe <dariofelipe91@gmail.com>
2021-04-27 22:21:13 -0300

    refactor(map): add current user location on map using expo-location

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
 import { MapContainer, MapContent } from './style';
 
 const mapStyle = {
@@ -7,25 +8,66 @@ const mapStyle = {
     width: '100%',
     height: '100%',
   },
+  mapConfig: [
+    {
+      featureType: 'poi',
+      stylers: [
+        {
+          visibility: 'off',
+        },
+      ],
+    },
+    {
+      featureType: 'transit',
+      stylers: [
+        {
+          visibility: 'off',
+        },
+      ],
+    },
+  ],
 };
 
-const Map = () => (
-  <>
-    <MapContainer>
-      <MapContent>
-        <MapView
-          style={mapStyle.map}
-          region={{
-            latitude: -8.0548874,
-            longitude: -34.8885838,
-            latitudeDelta: 0.03,
-            longitudeDelta: 0.03,
-          }}
-          showsUserLocation
-        />
-      </MapContent>
-    </MapContainer>
-  </>
-);
+const Map = () => {
+  const [location, setLocation] = useState({
+    latitude: -8.0548874,
+    longitude: -34.8885838,
+  });
+
+  const getLocation = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== 'granted') {
+      alert('A permissão de localização foi negada, por favor habilite-a.');
+    } else {
+      const position = await Location.getCurrentPositionAsync({});
+      setLocation(position.coords);
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return (
+    <>
+      <MapContainer>
+        <MapContent>
+          <MapView
+            style={mapStyle.map}
+            customMapStyle={mapStyle.mapConfig}
+            region={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+              latitudeDelta: 0.03,
+              longitudeDelta: 0.03,
+            }}
+            showsUserLocation
+          />
+        </MapContent>
+      </MapContainer>
+    </>
+  );
+};
 
 export default Map;

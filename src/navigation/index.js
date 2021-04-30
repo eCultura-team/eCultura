82d4e609f2b60892494308518ef08bc008bf4bd3
Dario-Felipe <dariofelipe91@gmail.com>
2021-04-29 23:28:24 -0300

    refactor(navigation): Adjust initial page

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AsyncStorage } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { colors } from '../tokens';
 import Logout from '../components/Logout';
 import Main from '../pages/Main';
+import Begin from '../pages/Begin';
 
 const Stack = createStackNavigator();
 
@@ -14,21 +16,52 @@ const NavigationStyle = {
   },
 };
 
-const Navigation = () => (
-  <>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={NavigationStyle}>
-        <Stack.Screen
-          name="Home"
-          component={Main}
-          options={{
-            headerLeft: () => <Logout />,
-            title: '',
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  </>
-);
+const Navigation = () => {
+  const [userName, setUserName] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function isStored() {
+    try {
+      const name = await AsyncStorage.getItem('1');
+      setUserName(name);
+      setIsLoading(false);
+    } catch (error) {
+      alert('Nome não registrado.');
+    }
+  }
+
+  useEffect(() => {
+    isStored();
+  }, []);
+
+  return (
+    <>
+      {isLoading ? null : (
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName={userName === null ? 'Begin' : 'Home'}
+            screenOptions={NavigationStyle}
+          >
+            <Stack.Screen
+              name="Begin"
+              component={Begin}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Home"
+              component={Main}
+              options={{
+                headerLeft: () => <Logout />,
+                title: '',
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      )}
+    </>
+  );
+};
 
 export default Navigation;

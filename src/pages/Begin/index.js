9ff003abbe17b04f6e19378c5f88e35d9825b685
Dario-Felipe <dariofelipe91@gmail.com>
2021-04-29 23:29:58 -0300

    feat(begin): Add new page for new users register yours name

---
+++ src/pages/Begin/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Button, AsyncStorage } from 'react-native';
+import * as S from './style';
+import logo from '../../assets/logo.png';
+import BeginBackground from '../../assets/BeginBackground.png';
+
+const Begin = ({ navigation }) => {
+  const [userName, setUserName] = useState('');
+
+  const saveName = async () => {
+    try {
+      if (userName.length === 0) {
+        alert('Ops, parece que você esqueceu de digitar seu nome.😕');
+      } else {
+        await AsyncStorage.setItem('1', userName);
+        navigation.navigate('Home');
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <>
+      <S.BeginHeader>
+        <S.BeginHeaderBackground source={BeginBackground} />
+        <S.BeginHeaderLogo source={logo} />
+      </S.BeginHeader>
+      <S.BeginDescription>
+        <S.BeginDescriptionTitle>
+          Seu marketplace para encontrar locais históricos.
+        </S.BeginDescriptionTitle>
+        <S.BeginDescriptionText>
+          Ajudamos você a achar lugares históricos de forma rápida e eficiente.
+        </S.BeginDescriptionText>
+      </S.BeginDescription>
+      <S.BeginName>
+        <S.BeginNameInput
+          placeholder="Como podemos te chamar?"
+          onChangeText={(text) => setUserName(text)}
+        />
+        <Button title="Entrar" onPress={() => saveName()} />
+      </S.BeginName>
+    </>
+  );
+};
+
+export default Begin;

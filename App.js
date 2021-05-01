cd0d7c356f7d28f52497e6089a9c24bf03a31c82
Dario-Felipe <dariofelipe91@gmail.com>
2021-04-30 21:28:25 -0300

    style: Adjust statusbar color at all app

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import Navigation from './src/navigation/index';
 import Store from './src/providers/store';
+import { colors } from './src/tokens';
 
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" backgroundColor={colors.primary} />
       <Store>
         <Navigation />
       </Store>

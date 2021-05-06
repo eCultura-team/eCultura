97dc759bee58d0beea9db4783449b0838b4e6a13
Dario-Felipe <dariofelipe91@gmail.com>
2021-05-05 21:14:10 -0300

    refactor(categorylist): adjust listing locations per categories

--- src/pages/CategoryList/index.js
+++ src/pages/CategoryList/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FlatList, Text } from 'react-native';
 import CategoryTitle from '../../components/CategoryTitle';
+import { useStore } from '../../providers/store';
 
-const CategoryList = ({ route }) => (
-  <>
-    <CategoryTitle>{route.params.title}</CategoryTitle>
-  </>
-);
+const CategoryList = ({ route }) => {
+  const { museumResults, theatreResults, marketResults } = useStore();
+  const [currentCategory, setCurrentCategory] = useState([]);
+
+  const getCurrentCategory = (title) => {
+    switch (title) {
+      case 'Teatro':
+        setCurrentCategory(theatreResults);
+        break;
+
+      case 'Museu':
+        setCurrentCategory(museumResults);
+        break;
+
+      default:
+        setCurrentCategory(marketResults);
+        break;
+    }
+  };
+
+  useEffect(() => {
+    getCurrentCategory(route.params.title);
+  }, []);
+
+  return (
+    <>
+      <CategoryTitle>{route.params.title}</CategoryTitle>
+      <FlatList
+        data={currentCategory}
+        keyExtractor={(item) => item.nome}
+        renderItem={({ item }) => <Text>{item.nome}</Text>}
+      />
+    </>
+  );
+};
 
 export default CategoryList;

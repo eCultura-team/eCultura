2f335da80300d806f7f189747cc67c39477d8ff5
Ikaro <ikaro.tiagonas@gmail.com>
2021-05-07 20:46:43 -0300

    refactor(categorylist): add new filter and input for category list.

--- src/pages/CategoryList/index.js
+++ src/pages/CategoryList/index.js
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { FlatList } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { FlatList, Image } from 'react-native';
 import CategoryTitle from '../../components/CategoryTitle';
 import CategoryItem from '../../components/CategoryItem';
 import { useStore } from '../../providers/store';
+import * as S from './style';
+import search from '../../assets/search.png';
 
 const CategoryList = ({ route }) => {
   const { museumResults, theatreResults, marketResults } = useStore();
   const [currentCategory, setCurrentCategory] = useState([]);
+  const [inputText, setInputText] = useState([]);
+  const refInput = useRef(null);
 
   const getCurrentCategory = (title) => {
     switch (title) {
@@ -24,6 +28,13 @@ const CategoryList = ({ route }) => {
     }
   };
 
+  const filterPlaces = (array) => {
+    const result = array.filter((item) =>
+      item.nome.toLowerCase().includes(inputText),
+    );
+    return result;
+  };
+
   useEffect(() => {
     getCurrentCategory(route.params.title);
   }, []);
@@ -31,17 +42,44 @@ const CategoryList = ({ route }) => {
   return (
     <>
       <CategoryTitle>{route.params.title}</CategoryTitle>
-      <FlatList
-        style={{
-          marginTop: 41,
-        }}
-        contentContainerStyle={{
-          alignSelf: 'center',
-        }}
-        data={currentCategory}
-        keyExtractor={(item) => item.nome}
-        renderItem={({ item }) => <CategoryItem title={item.nome} />}
-      />
+      <S.CategoryListContainer>
+        <S.CategorySearch>
+          <S.CategorySearchInput
+            placeholder="Pesquise..."
+            ref={refInput}
+            onChangeText={(text) => setInputText(text.toLowerCase())}
+          />
+          <S.CategorySearchContent
+            underlayColor="transparent"
+            onPress={() => refInput.current.focus()}
+          >
+            <Image source={search} />
+          </S.CategorySearchContent>
+        </S.CategorySearch>
+      </S.CategoryListContainer>
+      {filterPlaces(currentCategory).length === 0 ? (
+        <S.EmptyPlace>
+          <S.EmptyPlaceText>
+            Nenhuma localização encontrada. 😥
+          </S.EmptyPlaceText>
+        </S.EmptyPlace>
+      ) : (
+        <FlatList
+          style={{
+            marginTop: 41,
+          }}
+          contentContainerStyle={{
+            alignSelf: 'center',
+          }}
+          data={
+            inputText.length === 0
+              ? currentCategory
+              : filterPlaces(currentCategory)
+          }
+          keyExtractor={(item) => item.nome}
+          renderItem={({ item }) => <CategoryItem title={item.nome} />}
+        />
+      )}
     </>
   );
 };

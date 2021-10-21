88e9d326ef584f2381c7ae92ff68b1968d3e2e95
Dario-Felipe <dariofelipe91@gmail.com>
2021-10-21 19:29:30 -0300

    refactor(button): adjust disabled option and style when is disabled

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,18 +2,19 @@ import { Linking, Image, TouchableHighlight } from 'react-native';
 import React from 'react';
 import * as S from './style';
 
-const Button = ({ children, url, handle, icon }) => (
+const Button = ({ children, url, handle, icon, disabled }) => (
   <>
     <S.ButtonContainer>
       <TouchableHighlight
         underlayColor="transparent"
         onPress={url ? () => Linking.openURL(url) : handle}
+        disabled={disabled}
       >
         <S.ButtonContent>
-          <S.ButtonContentIcon>
+          <S.ButtonContentIcon disabled={disabled}>
             <Image source={icon} />
           </S.ButtonContentIcon>
-          <S.ButtonContentText>
+          <S.ButtonContentText disabled={disabled}>
             <S.ButtonContentTextTitle>{children}</S.ButtonContentTextTitle>
           </S.ButtonContentText>
         </S.ButtonContent>

a68785bfaa056f59a3d33cfe6fcca177c204dea4
Dario-Felipe <dariofelipe91@gmail.com>
2021-11-04 19:57:02 -0300

    feat: add FMC for notifications

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,7 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import Constants from 'expo-constants';
 import { ScrollView, TouchableHighlight, Image } from 'react-native';
+import * as Notifications from 'expo-notifications';
 import iconUser from '../../assets/iconUser.png';
 
 import Welcome from '../../components/Welcome';
@@ -8,9 +10,49 @@ import CategoryMenu from '../../components/CategoryMenu';
 import { Box } from './styles';
 import Modal from '../../components/Modal';
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
 const Main = ({ navigation }) => {
   const [errorSend, setErrorSend] = useState(false);
+  const [errorNotification, setErrorNotification] = useState(false);
   const control = useRef(null);
+  const controlNotificaton = useRef(null);
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+
+  async function registerForPushNotificationsAsync() {
+    const {
+      status: existingStatus,
+    } = await Notifications.getPermissionsAsync();
+
+    let finalStatus = existingStatus;
+
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+
+    if (finalStatus !== 'granted') {
+      setErrorNotification('Failed to get push token for push notification!');
+      return;
+    }
+
+    const token = (await Notifications.getExpoPushTokenAsync()).data;
+
+    return token;
+  }
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token),
+    );
+  }, []);
 
   return (
     <>
@@ -30,11 +72,19 @@ const Main = ({ navigation }) => {
         />
         <CategoryMenu navigation={navigation} />
       </ScrollView>
+
       <Modal
         control={control}
         buttonMessage="Fechar"
         error={errorSend}
         handle={() => control.current?.close()}
+      />
+
+      <Modal
+        control={controlNotificaton}
+        buttonMessage="Fechar"
+        error={errorNotification}
+        handle={() => controlNotificaton.current?.close()}
       />
     </>
   );
